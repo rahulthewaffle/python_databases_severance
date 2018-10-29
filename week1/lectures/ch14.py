ecb311e26356066f9b63c71678189a9a9e8f807d
rahulthewaffle <rahul.sairam@gmail.com>
2018-10-29 16:22:56 -0700

    Completed ch14 lecture notes

--- week1/lectures/ch14.py
+++ week1/lectures/ch14.py
@@ -1,8 +1,11 @@
 #object methods
 
 test = 'abc'
-type(test) #prints type of object
-dir(test) #lists all object methods
+x = type(test) #type of object
+y = dir(test) #lists all object methods
+
+print(x)
+print(y)
 
 #object test with PartyAnimal
 
@@ -26,8 +29,23 @@ j = PartyAnimal("Jim")
 
 s.party()
 s.party()
-j.party
+j.party()
 s.party()
 
 s = 42
 print('s contains', s)
+
+class FootballFan(PartyAnimal) :
+    points = 0
+
+    def touchdown(self) :
+        self.points = self.points + 7
+        self.party = self.party()
+        print(self.name, 'points', self.points)
+
+s = PartyAnimal('Sally')
+s.party()
+
+j = FootballFan('Jim')
+j.party()
+j.touchdown()
