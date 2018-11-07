864bacd9696df958580464e64b50234dde3fe487
rahulthewaffle <rahul.sairam@gmail.com>
2018-11-07 13:48:50 -0800

    Added correct entry for Eastlake HS to where.data, performed geoload and dump on DB and json

---
+++ week5/geodata/where.js
@@ -0,0 +1,12 @@
+myData = [
+[32.6402859,-116.9675992, '1120 Eastlake Pkwy, Chula Vista, CA 91915, USA'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، 11835, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[47.6135603,-122.0313473, '400 228th Ave NE, Sammamish, WA 98074, USA'],
+[38.0399391,23.8030901, 'Monumental Plaza, Building C, 1st Floor, Leof. Kifisias 44, Marousi 151 25, Greece'],
+[28.3639976,75.58696809999999, 'VidyaVihar Campus, street number 41, Pilani, Rajasthan 333031, India'],
+[6.8946472,3.7174267, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India']
+];
